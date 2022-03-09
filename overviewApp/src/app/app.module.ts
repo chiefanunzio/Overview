9bc2bfe734672b438ad21114b324fe913aee839d
nunzio <chiefa.nunzio@gmail.com>
2022-03-09 15:52:26 +0100

    creazione primi componenti

--- overviewApp/src/app/app.module.ts
+++ overviewApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ClientComponent } from './components/client/client.component';
+import { ProjectComponent } from './components/project/project.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ClientComponent,
+    ProjectComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,

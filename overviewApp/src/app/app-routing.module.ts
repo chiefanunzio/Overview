618583ae475b5da7fe6ecbac4d253f289703bf55
Riccarpa <riccardopagni99@gmail.com>
2022-03-15 10:55:24 +0100

    added users route

--- overviewApp/src/app/app-routing.module.ts
+++ overviewApp/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { ProjectComponent } from './components/project/project.component';
 import { UpdateProjectComponent } from './components/project/update-project/update-project.component';
 import { UserComponent } from './components/user/user.component';
 import { UpdateClientComponent } from './components/client/update-client/update-client.component';
+import { UpdateUserComponent } from './components/user/update-user/update-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'client', component: ClientComponent },
   { path: 'user', component: UserComponent },
+  { path: 'user/:id', component: UpdateUserComponent },
   { path: 'project', component: ProjectComponent },
   { path: 'updateProject/:id', component: UpdateProjectComponent },
   { path: 'client/:id', component: UpdateClientComponent },
